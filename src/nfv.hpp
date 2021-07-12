07b07a11476f251a09c1ba4f016baef6e4c32e22
Tianjiao Huang <git@tjhu.dev>
2021-07-11 18:23:30 -0600

    better extern c

--- src/nfv.hpp
+++ src/nfv.hpp
@@ -1,23 +1,23 @@
 #include <rte_mbuf.h>
 
 #ifdef __cplusplus
-extern "C" typedef void (*nfv_fn_t)(rte_mbuf *m);
-#else
+extern "C" {
+#endif
+
+// Function pointer type of a network function.
 typedef void (*nfv_fn_t)(struct rte_mbuf *m);
-#endif
 
-// NF1: decrement TTL
-#ifdef __cplusplus
-extern "C" void nf1_decrement_ttl(rte_mbuf *m);
-#else
+// NF1: decrement TTL.
 void nf1_decrement_ttl(struct rte_mbuf *m);
-#endif
 
-// NF2: one way NAT
-#ifdef __cplusplus
-extern "C" void nf2_init();
-extern "C" void nf2_one_way_nat(rte_mbuf *m);
-#else
+// NF2: one way NAT.
 void nf2_init();
 void nf2_one_way_nat(struct rte_mbuf *m);
+
+// NF3: ACL.
+void nf3_init();
+void nf2_one_way_nat(struct rte_mbuf *m);
+
+#ifdef __cplusplus  
+}
 #endif
