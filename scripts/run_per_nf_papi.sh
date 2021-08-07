5a1a81ccc4012952129f25ac85a2275e73187fbe
Tianjiao Huang <git@tjhu.dev>
2021-08-06 19:16:37 -0500

    fix papi

--- scripts/run_per_nf_papi.sh
+++ scripts/run_per_nf_papi.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -euo pipefail
-OUTDIR="log/papi/"
+OUTDIR="log/"
 OUTDIR="${OUTDIR}/$(date +"%m-%d-%H-%M-%S")_papi_out/"
+OUTFILE="${OUTDIR}/$(date +"%m-%d-%H-%M-%S")_out"
 BUILD_DIR=build_papi/
+PAPI_EVENTS="PAPI_TOT_INS,PAPI_TOT_CYC,PAPI_BR_MSP,PAPI_BR_PRC,PAPI_BR_CN"
 
 mkdir -p ${OUTDIR}
 
@@ -13,7 +15,7 @@ fi
 ninja -C ${BUILD_DIR}
 
 run() {
-  sudo PAPI_OUTPUT_DIRECTORY=$2 ./$1/$2 -- --portmask=1 --max_packets=2 --network_functions=$3 | tail -2 >> ${OUTFILE}
+  sudo PAPI_OUTPUT_DIRECTORY=$OUTDIR/$2 PAPI_EVENTS=$PAPI_EVENTS ./$1/$2 -- --portmask=1 --max_packets=2 --network_functions=$3 | tail -2 >> ${OUTFILE}
 }
 
 for nf in 1 2 3 4
