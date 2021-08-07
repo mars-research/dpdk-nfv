dc9a4ada40d4964e2224038089cd18a134f6045b
Tianjiao Huang <git@tjhu.dev>
2021-08-06 21:52:01 -0500

    better run_papi

--- scripts/run_per_nf_papi.sh
+++ scripts/run_per_nf_papi.sh
@@ -15,7 +15,7 @@ fi
 ninja -C ${BUILD_DIR}
 
 run() {
-  sudo PAPI_OUTPUT_DIRECTORY=$OUTDIR/$2 PAPI_EVENTS=$PAPI_EVENTS ./$1/$2 -- --portmask=1 --max_packets=2 --network_functions=$3 | tail -2 >> ${OUTFILE}
+  sudo PAPI_OUTPUT_DIRECTORY=${OUTDIR}/$2/$3 PAPI_EVENTS=$PAPI_EVENTS ./$1/$2 -- --portmask=1 --max_packets=2 --network_functions=$3 | tail -2 >> ${OUTFILE}
 }
 
 for nf in 1 2 3 4
@@ -23,7 +23,7 @@ do
   echo "NF${nf}" >> ${OUTFILE}
   for name in 'notrampoline' 'rust'
   do
-    for burst in 1 32
+    for burst in 1
     do
       bin_name="nfv-${name}-${burst}"
       run ${BUILD_DIR} ${bin_name} ${nf}
