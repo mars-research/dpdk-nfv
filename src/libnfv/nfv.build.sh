f2cc83e0efcbc5027b22aa177d26157ab57f6fd2
FeizaiYiHao <xiangdc@bu.edu>
2022-08-23 15:10:02 -0500

    fix unused build

--- src/libnfv/nfv.build.sh
+++ src/libnfv/nfv.build.sh
@@ -6,5 +6,4 @@ gcc -g -O3 -fno-stack-protector -shared -nostdlib -o nf1.so nf1.c
 gcc -g -O3 -fno-stack-protector -shared -nostdlib -o nf2.so nf2.c hashmap.c packettool.c
 gcc -g -O3 -fno-stack-protector -shared -nostdlib -o nf3.so nf3.c hashmap.c packettool.c
 gcc -g -O3 -fno-stack-protector -shared -nostdlib -o nf4.so nf4.c maglev.c packettool.c
-gcc -o nf3_test nf3_test.c nf3.so
 echo success!
