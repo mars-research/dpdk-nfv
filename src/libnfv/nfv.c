7289cdcf0677ef4d15de6be01dc568a899d063b9
FeizaiYiHao <xiangdc@bu.edu>
2022-09-05 19:18:20 -0500

    should be working

--- src/libnfv/nfv.c
+++ src/libnfv/nfv.c
@@ -17,7 +17,9 @@ extern Buddy_Allocator *buddy_b;
 extern Buddy_Allocator *buddy_c;
 extern Buddy_Allocator *buddy_d;
 void* alloc_1(size_t request ){
-return buddy_alloc( request,buddy_a);
+	void * addr = buddy_alloc( request,buddy_a);
+ printf("addr %p\n",addr);
+ return addr; 
 }
 
 void* alloc_2(size_t request ){
@@ -38,7 +40,7 @@ void Load_nf1(){
 	extern int CURRENT_DOM;
     extern int pkey_a;
     CURRENT_DOM = 1;
-  	struct elf_domain * elf_a = load_elf_domain("/users/BUXD/DOIY/dpdk-nfv/src/libnfv/nf1.so", alloc_1, pkey_a);
+  	struct elf_domain * elf_a = load_elf_domain("src/libnfv/nf1.so", alloc_1, pkey_a);
  	 _nf1 = find_symbol(elf_a, "process_frames");
 	 CURRENT_DOM = 0;
  }
@@ -46,7 +48,7 @@ void Load_nf1(){
 	extern int CURRENT_DOM;
     extern int pkey_b;
     CURRENT_DOM = 2;
-  	struct elf_domain * elf_a = load_elf_domain("/users/BUXD/DOIY/dpdk-nfv/src/libnfv/nf2.so", alloc_2, pkey_b);
+  	struct elf_domain * elf_a = load_elf_domain("src/libnfv/nf2.so", alloc_2, pkey_b);
  	 _nf2 = find_symbol(elf_a, "process_frames");
 	 CURRENT_DOM = 0;
  }
@@ -54,7 +56,7 @@ void Load_nf1(){
 	extern int CURRENT_DOM;
     extern int pkey_c;
     CURRENT_DOM = 3;
-  	struct elf_domain * elf_a = load_elf_domain("/users/BUXD/DOIY/dpdk-nfv/src/libnfv/nf3.so", alloc_3, pkey_c);
+  	struct elf_domain * elf_a = load_elf_domain("src/libnfv/nf3.so", alloc_3, pkey_c);
  	 _nf3 = find_symbol(elf_a, "process_frames");
 	 CURRENT_DOM = 0;
  }
@@ -62,7 +64,7 @@ void Load_nf1(){
 	extern int CURRENT_DOM;
     extern int pkey_d;
     CURRENT_DOM = 4;
-  	struct elf_domain * elf_a = load_elf_domain("/users/BUXD/DOIY/dpdk-nfv/src/libnfv/nf4.so", alloc_4, pkey_d);
+  	struct elf_domain * elf_a = load_elf_domain("src/libnfv/nf4.so", alloc_4, pkey_d);
  	 _nf4 = find_symbol(elf_a, "process_frames");
 	 nf4_init = find_symbol(elf_a, "maglev_init");
 	 nf4_init();
