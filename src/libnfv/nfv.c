2c40836b8ac865df24b3f8454a42026edbf07c89
FeizaiYiHao <xiangdc@bu.edu>
2022-09-06 18:59:53 -0600

    now DPDK needs no more update (hopefully)

--- src/libnfv/nfv.c
+++ src/libnfv/nfv.c
@@ -7,40 +7,23 @@ size_t (*_nf4)(struct rte_ether_hdr** packets,int nb_rx, int not_used,int buffer
 void (*nf4_init)();
 
 void run_nfs(struct rte_ether_hdr **packets, uint64_t pkt_len) {
+
     nf1(packets,pkt_len,0,13);
+
     nf2(packets,pkt_len,0,13);
+
     nf3(packets,pkt_len,0,13);
+
     nf4(packets,pkt_len,0,13);
-}
-extern Buddy_Allocator *buddy_a;
-extern Buddy_Allocator *buddy_b;
-extern Buddy_Allocator *buddy_c;
-extern Buddy_Allocator *buddy_d;
-void* alloc_1(size_t request ){
-	void * addr = buddy_alloc( request,buddy_a);
- printf("addr %p\n",addr);
- return addr; 
-}
 
-void* alloc_2(size_t request ){
-return buddy_alloc( request,buddy_b);
 }
-
-void* alloc_3(size_t request ){
-return buddy_alloc( request,buddy_c);
-}
-
-void* alloc_4(size_t request ){
-return buddy_alloc( request,buddy_d);
-}
-
 
 
 void Load_nf1(){
 	extern int CURRENT_DOM;
     extern int pkey_a;
     CURRENT_DOM = 1;
-  	struct elf_domain * elf_a = load_elf_domain("src/libnfv/nf1.so", alloc_1, pkey_a);
+  	struct elf_domain * elf_a = load_elf_domain("src/libnfv/nf1.so", pkey_a);
  	 _nf1 = find_symbol(elf_a, "process_frames");
 	 CURRENT_DOM = 0;
  }
@@ -48,7 +31,7 @@ void Load_nf1(){
 	extern int CURRENT_DOM;
     extern int pkey_b;
     CURRENT_DOM = 2;
-  	struct elf_domain * elf_a = load_elf_domain("src/libnfv/nf2.so", alloc_2, pkey_b);
+  	struct elf_domain * elf_a = load_elf_domain("src/libnfv/nf2.so", pkey_b);
  	 _nf2 = find_symbol(elf_a, "process_frames");
 	 CURRENT_DOM = 0;
  }
@@ -56,7 +39,7 @@ void Load_nf1(){
 	extern int CURRENT_DOM;
     extern int pkey_c;
     CURRENT_DOM = 3;
-  	struct elf_domain * elf_a = load_elf_domain("src/libnfv/nf3.so", alloc_3, pkey_c);
+  	struct elf_domain * elf_a = load_elf_domain("src/libnfv/nf3.so", pkey_c);
  	 _nf3 = find_symbol(elf_a, "process_frames");
 	 CURRENT_DOM = 0;
  }
@@ -64,7 +47,7 @@ void Load_nf1(){
 	extern int CURRENT_DOM;
     extern int pkey_d;
     CURRENT_DOM = 4;
-  	struct elf_domain * elf_a = load_elf_domain("src/libnfv/nf4.so", alloc_4, pkey_d);
+  	struct elf_domain * elf_a = load_elf_domain("src/libnfv/nf4.so", pkey_d);
  	 _nf4 = find_symbol(elf_a, "process_frames");
 	 nf4_init = find_symbol(elf_a, "maglev_init");
 	 nf4_init();
