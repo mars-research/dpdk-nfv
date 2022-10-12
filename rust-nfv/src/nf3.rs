9cf3402e72149193a3526bbbc785e763d961494e
Zhaofeng Li <hello@zhaofeng.li>
2022-10-11 19:00:36 -0500

    rust-nfv: Update nf3 logic to match C

--- rust-nfv/src/nf3.rs
+++ rust-nfv/src/nf3.rs
@@ -27,24 +27,10 @@ pub struct Acl {
 
 impl Acl {
     pub fn matches(&self, flow: &Flow, connections: &FlowHashSet) -> bool {
-        if (self.src_ip.is_none() || self.src_ip.unwrap() == (flow.src_ip))
+        (self.src_ip.is_none() || self.src_ip.unwrap() == (flow.src_ip))
             && (self.dst_ip.is_none() || self.dst_ip.unwrap() == (flow.dst_ip))
             && (self.src_port.is_none() || flow.src_port == self.src_port.unwrap())
             && (self.dst_port.is_none() || flow.dst_port == self.dst_port.unwrap())
-        {
-            if let Some(established) = self.established {
-                let rev_flow = flow.reverse_flow();
-                #[cfg(feature = "use_hashbrown")]
-                return (connections.contains(flow) || connections.contains(&rev_flow)) == established;
-
-                #[cfg(not(feature = "use_hashbrown"))]
-                return connections.get(flow).is_some() || connections.get(&rev_flow).is_some() == established;
-            } else {
-                true
-            }
-        } else {
-            false
-        }
     }
 }
 
@@ -73,8 +59,11 @@ impl crate::nfv::NetworkFunction for Nf3Acl {
         for pkt in packets.iter_mut() {
             let flow = pkt.get_flow();
 
+
+            let mut matched = false;
             for acl in &self.acls {
                 if acl.matches(&flow, &self.flow_cache) {
+                    matched = true;
                     if !acl.drop {
                         #[cfg(feature = "use_hashbrown")]
                         self.flow_cache.insert(flow);
@@ -85,6 +74,22 @@ impl crate::nfv::NetworkFunction for Nf3Acl {
                     //return !acl.drop;
                 }
             }
+
+            if !matched {
+                let reverse_flow = flow.reverse_flow();
+
+                #[cfg(feature = "use_hashbrown")]
+                let contains_flow = self.flow_cache.contains(&reverse_flow);
+
+                #[cfg(not(feature = "use_hashbrown"))]
+                let contains_flow = self.flow_cache.get(&reverse_flow).is_some();
+
+                if contains_flow {
+                    matched = true;
+                }
+            }
+
+            std::hint::black_box(matched);
         }
     }
 }
